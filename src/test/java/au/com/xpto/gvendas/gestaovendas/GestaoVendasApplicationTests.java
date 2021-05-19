1187ed2853d114b457044c61127ef7123c29ca44
henriquedeof <henriquedeof@gmail.com>
2021-05-19 21:24:19 +1000

    First commit.

---
+++ src/test/java/au/com/xpto/gvendas/gestaovendas/GestaoVendasApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.xpto.gvendas.gestaovendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoVendasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
